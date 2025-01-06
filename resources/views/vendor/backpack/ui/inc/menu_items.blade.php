48ba66a7738dc352a387723eec5b8e5c3b5cf2d2
mpa12 <maksim.28.05@mail.ru>
2025-01-07 01:29:14 +1000

    feat: Updated menu

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -6,8 +6,9 @@
 </li>
 
 <x-backpack::menu-dropdown title="Add-ons" icon="la la-puzzle-piece">
-    <x-backpack::menu-dropdown-header title="Authentication"/>
     <x-backpack::menu-dropdown-item title="Users" icon="la la-user" :link="backpack_url('user')"/>
 </x-backpack::menu-dropdown>
 
-<x-backpack::menu-item title="Notes" icon="bi bi-music-note-list fs-1rem" :link="backpack_url('notes')" />
+<x-backpack::menu-dropdown title="Notes" icon="bi bi-shop fs-1rem">
+    <x-backpack::menu-item title="Notes" icon="bi bi-music-note-list fs-1rem" :link="backpack_url('notes')" />
+</x-backpack::menu-dropdown>
